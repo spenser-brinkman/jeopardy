a0584b9604c499984b72ad0d54694c97d00bd2a4
spenser6131 <brinkman.spenser@gmail.com>
2020-10-20 09:18:20 -0700

    validator mostly working

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,11 +8,10 @@ class CLI
     sleep(0)
     puts "Loading your categories, please wait.".center(172) 
     self.gap
-    self.gather_categories
+    self.gather_and_validate    
     self.prompt_for_input
     puts "Great! Please wait for your clues to load.".center(172)
     self.gap
-    Fetcher.fetch_thirty_clues
     self.display_board
     binding.pry
   end
@@ -20,9 +19,22 @@ class CLI
   def gather_categories
     Category.all.clear
     Fetcher.fetch_six_categories
-    puts "Done!".center(172)
+    puts "Categories loaded, checking validity of clues".center(172)
     self.gap
     sleep(0)
+  end
+
+  def gather_clues
+    Clue.all.clear
+    Fetcher.fetch_thirty_clues
+  end
+
+  def gather_and_validate
+    loop do
+      self.gather_categories
+      self.gather_clues
+      break if Clue.all[0..30].all? {|c| c.instance_variable_get(:@point_value) != nil}
+    end
   end
 
   def prompt_for_input
@@ -46,7 +58,7 @@ class CLI
         sleep(0)
         puts "Loading a new set of categories, please wait.".center(172)
         puts ""
-        self.gather_categories
+        self.gather_and_validate
       else
         sleep(0)
         puts ""
@@ -63,22 +75,22 @@ class CLI
 
 
   def generate_board
-    @row_one = ["#{Category.all[0].clues[0].point_value}", "#{Category.all[0].clues[1].point_value}", "#{Category.all[0].clues[2].point_value}", "#{Category.all[0].clues[3].point_value}", "#{Category.all[0].clues[4].point_value}", "#{Category.all[0].clues[5].point_value}", ]
-    @row_two = ["#{Category.all[1].clues[0].point_value}", "#{Category.all[1].clues[1].point_value}", "#{Category.all[1].clues[2].point_value}", "#{Category.all[1].clues[3].point_value}", "#{Category.all[1].clues[4].point_value}", "#{Category.all[1].clues[5].point_value}", ]
-    @row_three = ["#{Category.all[2].clues[0].point_value}", "#{Category.all[2].clues[1].point_value}", "#{Category.all[2].clues[2].point_value}", "#{Category.all[2].clues[3].point_value}", "#{Category.all[2].clues[4].point_value}", "#{Category.all[2].clues[5].point_value}", ]
-    @row_four = ["#{Category.all[3].clues[0].point_value}", "#{Category.all[3].clues[1].point_value}", "#{Category.all[3].clues[2].point_value}", "#{Category.all[3].clues[3].point_value}", "#{Category.all[3].clues[4].point_value}", "#{Category.all[3].clues[5].point_value}", ]
-    @row_five = ["#{Category.all[4].clues[0].point_value}", "#{Category.all[4].clues[1].point_value}", "#{Category.all[4].clues[2].point_value}", "#{Category.all[4].clues[3].point_value}", "#{Category.all[4].clues[4].point_value}", "#{Category.all[4].clues[5].point_value}", ]
-    @row_six = ["#{Category.all[5].clues[0].point_value}", "#{Category.all[5].clues[1].point_value}", "#{Category.all[5].clues[2].point_value}", "#{Category.all[5].clues[3].point_value}", "#{Category.all[5].clues[4].point_value}", "#{Category.all[5].clues[5].point_value}", ]
+    @row_one = ["#{Category.all[0].clues[0].point_value}", "#{Category.all[0].clues[1].point_value}", "#{Category.all[0].clues[2].point_value}", "#{Category.all[0].clues[3].point_value}", "#{Category.all[0].clues[4].point_value}"]
+    @row_two = ["#{Category.all[1].clues[0].point_value}", "#{Category.all[1].clues[1].point_value}", "#{Category.all[1].clues[2].point_value}", "#{Category.all[1].clues[3].point_value}", "#{Category.all[1].clues[4].point_value}"]
+    @row_three = ["#{Category.all[2].clues[0].point_value}", "#{Category.all[2].clues[1].point_value}", "#{Category.all[2].clues[2].point_value}", "#{Category.all[2].clues[3].point_value}", "#{Category.all[2].clues[4].point_value}"]
+    @row_four = ["#{Category.all[3].clues[0].point_value}", "#{Category.all[3].clues[1].point_value}", "#{Category.all[3].clues[2].point_value}", "#{Category.all[3].clues[3].point_value}", "#{Category.all[3].clues[4].point_value}"]
+    @row_five = ["#{Category.all[4].clues[0].point_value}", "#{Category.all[4].clues[1].point_value}", "#{Category.all[4].clues[2].point_value}", "#{Category.all[4].clues[3].point_value}", "#{Category.all[4].clues[4].point_value}"]
+    @row_six = ["#{Category.all[5].clues[0].point_value}", "#{Category.all[5].clues[1].point_value}", "#{Category.all[5].clues[2].point_value}", "#{Category.all[5].clues[3].point_value}", "#{Category.all[5].clues[4].point_value}"]
   end
 
   def display_board
     self.generate_board
-    puts "#{Category.all[1].name}    ---    #{@row_one[0]} | #{@row_one[1]} | #{@row_one[2]} | #{@row_one[3]} | #{@row_one[4]} | #{@row_one[5]}".center(172)
-    puts "#{Category.all[2].name}    ---    #{@row_two[0]} | #{@row_two[1]} | #{@row_two[2]} | #{@row_two[3]} | #{@row_two[4]} | #{@row_two[5]}".center(172)
-    puts "#{Category.all[3].name}    ---    #{@row_three[0]} | #{@row_three[1]} | #{@row_three[2]} | #{@row_three[3]} | #{@row_three[4]} | #{@row_three[5]}".center(172)
-    puts "#{Category.all[4].name}    ---    #{@row_four[0]} | #{@row_four[1]} | #{@row_four[2]} | #{@row_four[3]} | #{@row_four[4]} | #{@row_four[5]}".center(172)
-    puts "#{Category.all[5].name}    ---    #{@row_five[0]} | #{@row_five[1]} | #{@row_five[2]} | #{@row_five[3]} | #{@row_five[4]} | #{@row_five[5]}".center(172)
-    puts "#{Category.all[0].name}    ---    #{@row_six[0]} | #{@row_six[1]} | #{@row_six[2]} | #{@row_six[3]} | #{@row_six[4]} | #{@row_six[5]}".center(172)
+    puts "#{Category.all[1].name}" + "    ---    ".center(172) + "#{@row_one[0]} | #{@row_one[1]} | #{@row_one[2]} | #{@row_one[3]} | #{@row_one[4]}"
+    puts "#{Category.all[2].name}    ---    #{@row_two[0]} | #{@row_two[1]} | #{@row_two[2]} | #{@row_two[3]} | #{@row_two[4]}".center(172)
+    puts "#{Category.all[3].name}    ---    #{@row_three[0]} | #{@row_three[1]} | #{@row_three[2]} | #{@row_three[3]} | #{@row_three[4]}".center(172)
+    puts "#{Category.all[4].name}    ---    #{@row_four[0]} | #{@row_four[1]} | #{@row_four[2]} | #{@row_four[3]} | #{@row_four[4]}".center(172)
+    puts "#{Category.all[5].name}    ---    #{@row_five[0]} | #{@row_five[1]} | #{@row_five[2]} | #{@row_five[3]} | #{@row_five[4]}".center(172)
+    puts "#{Category.all[0].name}    ---    #{@row_six[0]} | #{@row_six[1]} | #{@row_six[2]} | #{@row_six[3]} | #{@row_six[4]}".center(172)
   end
 
 
